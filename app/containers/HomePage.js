440dcb6d3ef3af25655909dec1226ca790ad83de
Jacob Haslehurst <jacob@haslehurst.net>
2017-03-03 20:20:26 +1100

    generate webp's of multiple sizes

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -2,8 +2,26 @@
 import React, {Component} from 'react';
 import Home from '../components/Home';
 import glob from 'glob';
+import sizeOf from 'image-size';
 const {dialog} = require('electron').remote;
 const {exec} = require('child_process');
+
+const SIZES = new Map([
+  ['thumbnail', 200],
+  ['large', 2000],
+  ['original', Infinity],
+]);
+
+function resizeToFitArg(dimensions, maxSize) {
+  if (maxSize === Infinity) {
+    return '';
+  }
+  if (dimensions.width > dimensions.height) {
+    return `-resize ${maxSize} 0`;
+  } else {
+    return `-resize 0 ${maxSize}`;
+  }
+}
 
 export default class HomePage extends Component {
   constructor(props) {
@@ -36,17 +54,31 @@ export default class HomePage extends Component {
   loadFiles() {
     console.log('loading files...');
     console.log(this.state.photosDirectory);
-    glob.glob(`${this.state.photosDirectory}/**/*.*`, (err, files) => {
+    glob.glob(`${this.state.photosDirectory}/**/*.dng`, {nocase: true}, (
+      err,
+      files,
+    ) => {
       console.log('files loaded', err);
       this.setState({files});
       for (let file of files) {
-        exec(`./bin/libraw/bin/dcraw_emu -W -T "${file}"`, () => {
-          exec(
-            `./bin/libwebp/examples/cwebp -q 85 -m 6 "${file}.tiff" -o "${file}.webp"`,
-            () => {
-              exec(`rm ""${file}.tiff"`);
-            },
-          );
+        exec(`nice -20 ./bin/libraw/bin/dcraw_emu -W -T "${file}"`, () => {
+          const dimensions = sizeOf(file);
+          console.log(dimensions);
+          for (let [label, size] of SIZES) {
+            exec(
+              `nice -20 ./bin/libwebp/examples/cwebp \
+                -preset photo \
+                ${resizeToFitArg(dimensions, size)} \
+                -q 80 \
+                -m 6 \
+                "${file}.tiff" \
+                -o "${file}.${label}.webp"
+              `,
+              err => {
+                console.log(err);
+              },
+            );
+          }
         });
         console.log(file);
       }
